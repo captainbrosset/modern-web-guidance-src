73e5e4e73dd085d2199c073bae2b6ce0848e0b2c
Rick Viscomi <rviscomi@google.com>
2026-01-06 20:50:20 -0500

    keep dest file permissions on deploy

--- evaluator/deploy.sh
+++ evaluator/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avP --exclude='.git' --exclude='node_modules' --exclude='.env' --exclude='.DS_Store' . /google/data/rw/users/rv/rviscomi/www/spike-test/
+rsync -rltgoDvP --exclude='.git' --exclude='node_modules' --exclude='.env' --exclude='.DS_Store' . /google/data/rw/users/rv/rviscomi/www/spike-test/
